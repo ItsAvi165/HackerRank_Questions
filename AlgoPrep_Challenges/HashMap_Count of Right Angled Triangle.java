1235c307fc7a024bc92b43ef16bd87d122584e3d
Avishkar Hongekar <69665805+ItsAvi165@users.noreply.github.com>
2024-02-23 08:23:06 +0530

    Added Comments in Count of Right Angled Triangle

--- AlgoPrep_Challenges/HashMap_Count of Right Angled Triangle.java
+++ AlgoPrep_Challenges/HashMap_Count of Right Angled Triangle.java
@@ -1,6 +1,6 @@
 //Important Question
 
-// Given an array arr[] of N distinct integers points on the 2D Plane. The task is to count the number of Right-Angled Triangle from N points such that 
+// Given an array arr[] of N distinct integers points on the 2D Plane. The task is to count the number of Right-Angled Triangle from N points such that
 // the base or perpendicular is parallel to the X or Y-axis.
 // Input Format
 
@@ -43,6 +43,7 @@ public class Solution {
         HashMap<Integer, Integer> xpoints = new HashMap<Integer, Integer>();
         HashMap<Integer, Integer> ypoints = new HashMap<Integer, Integer>();
 
+//Storing x co-ordinates in xpoints hashmap and y co-ordinate in ypoints hashmap
         for (int i = 0; i < n; i++) {
             if (xpoints.containsKey(points[i][0])) {
                 xpoints.put(points[i][0], xpoints.get(points[i][0]) + 1);
@@ -56,7 +57,12 @@ public class Solution {
             }
         }
         int count = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {// counting all triangles considering right angle is at each point
+
+          //count number of points whose x and y co-ordinates are same to the iterating point with which
+          //we can form the right angled triangles
+          //also -1 the point where the right angle is i.e iterating point
+
             if (xpoints.get(points[i][0]) >= 1 && ypoints.get(points[i][1]) >= 1) {
                 count += ((xpoints.get(points[i][0]) - 1) * (ypoints.get(points[i][1]) - 1));
             }
